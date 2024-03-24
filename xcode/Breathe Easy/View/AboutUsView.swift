c778df6b9512270a9130e3abf03673f33a69e6b5
nkumar041 <nikumar041@gmail.com>
2024-03-23 20:41:09 -0400

    new home page

---
+++ xcode/Breathe Easy/View/AboutUsView.swift
@@ -0,0 +1,149 @@
+//
+//  AboutUsView.swift
+//  Breathe Easy
+//
+//  Created by Nikhil Kumar on 3/23/24.
+//
+
+import SwiftUI
+
+struct AboutUsView: View {
+    @AppStorage("mainViewNum") var mainViewNum = 0
+//    @AppStorage("settingsTransition") var settingsTransition = ""
+    var body: some View {
+        ZStack() {
+            Button(action: goToSettings){
+                Image("BackArrowSettings").resizable().aspectRatio(contentMode: .fit) // Maintain the image's aspect ratio
+                    .frame(width: 20, height: 20)
+            }
+            .offset(x: -150, y: -340.50)
+            Group {
+                Text("About Us")
+                    .font(Font.custom("Lufga", size: 24))
+                    .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                    .offset(x: 0.50, y: -340.50)
+                ZStack() {
+                    HStack(spacing: 0) {
+                        Image("IconLung").resizable()
+                            .aspectRatio(contentMode: .fit).frame(width:90,height:80)
+                        
+                    }
+                    .padding(EdgeInsets(top: 26, leading: 25, bottom: 31, trailing: 25))
+                    .frame(width: 128, height: 128)
+                    .background(Color(red: 0, green: 0.32, blue: 0.27))
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color(red: 0.91, green: 0.91, blue: 0.91), lineWidth: 0.50)
+                    )
+                    .offset(x: 0, y: -39)
+                    Text("Breathe Easy")
+                        .font(Font.custom("Lufga", size: 24))
+                        .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                        .offset(x: 0, y: 67.50)
+                    Text("Version 1.0")
+                        .font(Font.custom("Aeonik TRIAL", size: 14))
+                        .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                        .offset(x: 0.50, y: 95)
+                }
+                .frame(width: 154, height: 206)
+                .offset(x: 0.50, y: -161)
+                Text("ABOUT US")
+                    .font(Font.custom("Aeonik TRIAL", size: 14))
+                    .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                    .offset(x: -128.50, y: -27)
+                Text("DEVELOPMENT TEAM")
+                    .font(Font.custom("Aeonik TRIAL", size: 14))
+                    .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                    .offset(x: -94, y: 245)
+                ZStack() {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 345, height: 236)
+                        .background(.white)
+                        .cornerRadius(15)
+                        .offset(x: 0, y: 0)
+                    ScrollView{
+                        Text("""
+Welcome to Breathe Easy, your trusted companion in the management of asthma. This documentation serves as an in-app guide to help you maximize the benefits of our application, designed to assist you in monitoring your peak flow and preventing asthma attacks.
+                 
+Breathe Easy boasts several key features that empower you in managing your asthma effectively. It provides real-time peak flow predictions powered by a machine learning regression model. The app leverages heart rate data from your Apple Watch and takes into account factors such as air quality and temperature to offer personalized insights.
+                 
+Breathe Easy is designed to deliver timely alerts and notifications, all seamlessly integrated with AWS for machine learning predictions.
+
+To get started with Breathe Easy, simply sign in using your existing account credentials. If you're a new user, you can effortlessly create an account to access all the app's features.
+
+Once you're inside the app, you'll be greeted by a user-friendly home screen displaying your current peak flow prediction and other essential information. You can review your past predictions and health insights in the trends tab. Breathe Easy continuously monitors your heart rate through watchOS data, and additionally measures other relevant factors with Apple’s WeatherKit to provide real-time peak flow predictions. It offers immediate feedback and compares your current peak flow to your maximum predicted value. It will alert you if your peak flow drops significantly, potentially indicating an asthma attack.
+
+You can easily customize your alert thresholds and notification preferences to suit your needs. The app will send timely alerts to your iOS device and Apple Watch, ensuring you're well-informed and can take necessary actions in the event your peak flow is at risk.
+
+Breathe Easy is here to provide you with the tools you need to manage your asthma with confidence and peace of mind. Thank you for choosing Breathe Easy.
+""")
+                        .font(Font.custom("Aeonik TRIAL", size: 14))
+                        .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                        .offset(x: 0.50, y: 0)
+                    }
+                    .frame(width: 325, height: 216)
+                }
+                .frame(width: 325, height: 216)
+                .offset(x: 0, y: 107)
+                ZStack() {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 325, height: 108)
+                        .background(.white)
+                        .cornerRadius(15)
+                        .offset(x: 0, y: 0)
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 300, height: 1)
+                            .background(Color(red: 0.91, green: 0.91, blue: 0.91))
+                            .offset(x: 0, y: 0)
+                        ZStack() {
+                            Text("Amogh Kuppa")
+                                .font(Font.custom("Lufga", size: 18))
+                                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                                .offset(x: -80.50, y: 0)
+                            Text("App Developer")
+                                .font(Font.custom("Aeonik TRIAL", size: 14))
+                                .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                                .offset(x: 96.50, y: 0.50)
+                        }
+                        .frame(width: 286, height: 23)
+                        .offset(x: -4, y: -28)
+                        ZStack() {
+                            Text("Nikhil Kumar")
+                                .font(Font.custom("Lufga", size: 18))
+                                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                                .offset(x: -87.50, y: 0)
+                            Text("App Developer")
+                                .font(Font.custom("Aeonik TRIAL", size: 14))
+                                .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                                .offset(x: 96.50, y: 0.50)
+                        }
+                        .frame(width: 286, height: 23)
+                        .offset(x: -4, y: 28)
+                    }
+                    .frame(width: 300, height: 79)
+                    .offset(x: 0.50, y: 0.50)
+                }
+                .frame(width: 325, height: 108)
+                .offset(x: 0, y: 319)
+            }
+        }
+        .frame(width: 1000, height: 1500)
+        .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+    }
+    
+    func goToSettings(){
+        withAnimation{
+//            settingsTransition = "fromAbout"
+            mainViewNum = 1
+        }
+    }
+}
+
+#Preview {
+    AboutUsView()
+}
--- xcode/Breathe Easy/View/SettingsView.swift
+++ xcode/Breathe Easy/View/SettingsView.swift
@@ -122,39 +122,6 @@ struct SettingsView: View {
                 }
                 .frame(width: 325, height: 160)
                 .offset(x: 0, y: -145)
-                
-//                ZStack() {
-//                    // fix the selector to be a circle
-//                    Rectangle()
-//                        .foregroundColor(.clear)
-//                        .frame(width: 28, height: 28)
-//                        .background(
-//                            Image("SettingsMoon").resizable()
-//                                .aspectRatio(contentMode: .fit)
-//                        )
-//                        .offset(x: -62, y: 0)
-//                    Text("Dark Mode")
-//                        .font(Font.custom("Lufga", size: 20))
-//                        .foregroundColor(Color(red: 0, green: 0.27, blue: 0.23))
-//                        .offset(x: 23, y: 0)
-//                }
-//                .offset(x: -73.50, y: -198)
-                
-//                ZStack() {
-//                    Rectangle()
-//                        .foregroundColor(.clear)
-//                        .frame(width: 38, height: 20)
-//                        .background(Color(red: 0.94, green: 0.94, blue: 0.94))
-//                        .cornerRadius(34)
-//                        .offset(x: 0, y: 0)
-//                    Ellipse()
-//                        .foregroundColor(Color(red: 0, green: 0.27, blue: 0.23))
-//                        .frame(width: 16, height: 16)
-//                        .background(.clear)
-//                        .offset(x: 9, y: 0)
-//                }
-//                .frame(width: 38, height: 20)
-//                .offset(x: 125.50, y: -198)
             }
         }
         .frame(width: 1000, height: 1500)
@@ -226,6 +193,217 @@ struct SettingsView: View {
     }
 }
 
+struct Settings: View {
+    @AppStorage("page") var page = 1
+    @AppStorage("progressPage") var progressPage = 1
+    @AppStorage("mainViewNum") var mainViewNum = 0
+//    @AppStorage("settingsTransition") var settingsTransition = ""
+    @AppStorage("uid") var uid = ""
+    @AppStorage("log_Status") var log_Status = false
+    @AppStorage("log_Status2") var log_Status2 = false
+    @AppStorage("fullname") var fullname = ""
+    var body: some View {
+        ZStack() {
+            Button(action: goToHome){
+                Image("BackArrowSettings").resizable().aspectRatio(contentMode: .fit) // Maintain the image's aspect ratio
+                    .frame(width: 20, height: 20)
+            }
+            .offset(x: -150, y: -340.50)
+            Text("Settings")
+                .font(Font.custom("Lufga", size: 24))
+                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                .offset(x: 0, y: -340.50)
+            ZStack() {
+                Text("PERSONALIZATION")
+                    .font(Font.custom("Aeonik TRIAL", size: 14))
+                    .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                    .offset(x: -103, y: -202.50)
+                Text("ACCOUNT")
+                    .font(Font.custom("Aeonik TRIAL", size: 14))
+                    .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                    .offset(x: -129, y: 56.50)
+                ZStack() {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 325, height: 109)
+                        .background(.white)
+                        .cornerRadius(15)
+                        .offset(x: 0, y: 0)
+                    ZStack() {
+                        ZStack() {
+                            Text("Appearance")
+                                .font(Font.custom("Lufga", size: 18))
+                                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                                .offset(x: -87.50, y: 0)
+                        }
+                        .frame(width: 285, height: 23)
+                        .offset(x: -0.50, y: -26.50)
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 300, height: 1)
+                            .background(Color(red: 0.91, green: 0.91, blue: 0.91))
+                            .offset(x: 0, y: -0.50)
+                        ZStack() {
+                            Text("Profile")
+                                .font(Font.custom("Lufga", size: 18))
+                                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                                .offset(x: -115, y: 0)
+                        }
+                        .frame(width: 285, height: 23)
+                        .offset(x: -0.50, y: 26.50)
+                    }
+                    .frame(width: 300, height: 76)
+                    .offset(x: 0.50, y: 0.50)
+                }
+                .frame(width: 325, height: 109)
+                .offset(x: 0.50, y: -126)
+                Button(action:logOut){
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 325, height: 56)
+                            .background(.white)
+                            .cornerRadius(15)
+                            .offset(x: 0, y: 0)
+                        ZStack() {
+                            Text("Sign Out")
+                                .font(Font.custom("Lufga", size: 18))
+                                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                                .offset(x: -106, y: 0)
+                        }
+                        .frame(width: 285, height: 23)
+                        .offset(x: 0, y: 0.50)
+                    }
+                }
+                .frame(width: 325, height: 56)
+                .offset(x: 0.50, y: 109.50)
+                
+                ZStack() {
+                    Text("INFORMATION")
+                        .font(Font.custom("Aeonik TRIAL", size: 14))
+                        .foregroundColor(Color(red: 0.48, green: 0.51, blue: 0.51))
+                        .offset(x: -117.50, y: -35)
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 325, height: 56)
+                            .background(.white)
+                            .cornerRadius(15)
+                            .offset(x: 0, y: 0)
+                        ZStack() {
+                            Text("About Us")
+                                .font(Font.custom("Lufga", size: 18))
+                                .foregroundColor(Color(red: 0, green: 0.32, blue: 0.27))
+                                .offset(x: -102.50, y: 0)
+                        }
+                        .frame(width: 285, height: 23)
+                        .offset(x: 0, y: 0.50)
+                    }
+                    .frame(width: 325, height: 56)
+                    .offset(x: 0, y: 15)
+                }
+                .frame(width: 325, height: 86)
+                .offset(x: 0.50, y: -11.50)
+                
+                Button(action:deleteAccount){
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 325, height: 56)
+                            .background(.white)
+                            .cornerRadius(15)
+                            .offset(x: 0, y: 0)
+                        Text("Delete Account")
+                            .font(Font.custom("Lufga", size: 18))
+                            .foregroundColor(Color(red: 0.73, green: 0.15, blue: 0.10))
+                            .offset(x: 0, y: -0.50)
+                    }
+                }
+                .frame(width: 325, height: 56)
+                .offset(x: 0.50, y: 182.50)
+                
+            }
+            .frame(width: 326, height: 421)
+            .offset(x: -0.50, y: -53.50)
+        }
+        .frame(width: 1000, height: 1500)
+        .background(Color(red: 0.97, green: 0.97, blue: 0.97));
+    }
+    
+    func goToHome(){
+        withAnimation{
+//            settingsTransition = "fromHome"
+            mainViewNum = 0
+        }
+    }
+    
+    func goToAbout(){
+        withAnimation{
+//            settingsTransition = "fromAbout"
+            mainViewNum = 2
+        }
+    }
+    
+    func deleteAccount(){
+        let user = Auth.auth().currentUser
+        user?.delete { error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Deletion Success")
+            }
+        }
+        
+        GIDSignIn.sharedInstance.signOut()
+        try? Auth.auth().signOut()
+        
+        log_Status = false
+        log_Status2 = false
+        page = 1
+        
+        let db = Firestore.firestore()
+        let documentRef = db.collection("users").document(uid)
+        
+        documentRef.delete { err in
+            if let err = err {
+                print("Error removing document: \(err)")
+            } else {
+                print("Document successfully removed!")
+            }
+        }
+        
+        fullname = ""
+        progressPage = 1
+        mainViewNum = 0
+    }
+    
+    func logOut(){
+        
+        if log_Status{
+            GIDSignIn.sharedInstance.signOut()
+            try? Auth.auth().signOut()
+            log_Status = false
+            page = 1
+            fullname = ""
+            progressPage = 1
+            mainViewNum = 0
+            
+        } else if log_Status2{
+            DispatchQueue.global(qos: .background).async {
+                
+                try? Auth.auth().signOut()
+            }
+            
+            log_Status2 = false
+            page = 1
+            fullname = ""
+            progressPage = 1
+            mainViewNum = 0
+        }
+    }
+}
+
 #Preview {
-    SettingsView()
+//    SettingsView()
+    Settings()
 }
